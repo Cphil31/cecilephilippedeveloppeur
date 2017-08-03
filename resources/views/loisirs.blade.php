25e9dbd81106a47aa6210cd9256e1931dfb10daf
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 23:22:27 +0200

    mise en page icones

--- resources/views/loisirs.blade.php
+++ resources/views/loisirs.blade.php
@@ -9,18 +9,19 @@
 
 @section('content')
 	<div class="loisirs"><a href="https://www.mixcloud.com/vito-sanacho/">DJ</a> dans des hôtels,clubs et soirées privées (2005-2012) </div>
-	<div class="loisirs">Sports : basket et footing en amateur</div>
-	<i class="github icon"></i>
-	<i class="gitlab icon"></i>
-	<i class="facebook icon"></i>
-	<i class="instagram icon"></i>
-	<i class="linkedin square icon"></i>
-	<i class="mixcloud icon"></i>
-	<i class="snapchat ghost icon"></i>
-	<i class="snapchat icon"></i>
-	<i class="snapchat square icon"></i>
-	<i class="soundcloud icon"></i>
-	<i class="twitter icon"></i>
-	<i class="twitter square icon"></i>
-	<i class="whatsapp icon"></i>
+	<div class="sport">Sports : basket et footing en amateur</div>
+		
+	<i class="github icon" style="font-size: 6em; "></i>
+	<i class="gitlab icon" style="font-size: 6em;"></i>
+	<i class="facebook icon" style="font-size: 6em;"></i>
+	<i class="instagram icon" style="font-size: 6em;"></i>
+	<i class="linkedin square icon" style="font-size: 6em;"></i>
+	<i class="mixcloud icon" style="font-size: 6em;"></i>
+	<i class="snapchat ghost icon" style="font-size: 6em;"></i>
+	<i class="snapchat icon" style="font-size: 6em;"></i>
+	<i class="snapchat square icon" style="font-size: 6em;"></i>
+	<i class="soundcloud icon" style="font-size: 6em;"></i>
+	<i class="twitter icon" style="font-size: 6em;"></i>
+	<i class="twitter square icon" style="font-size: 6em;"></i>
+	<i class="whatsapp icon" style="font-size: 6em;"></i>
 @endsection
