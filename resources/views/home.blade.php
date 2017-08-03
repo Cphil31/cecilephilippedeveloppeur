f286de96bdcda7b601f2b001780e98571d87af9d
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 17:47:46 +0200

    ajout layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
-<h1>hello world</h1>
+@extends('layouts.layout_home')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+    <p>This is my body content.</p>
+@endsection
