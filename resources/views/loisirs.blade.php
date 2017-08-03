17e279fd2031aa20d3c03cadb4301f7d53b17cf9
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 23:26:01 +0200

    mise en page a ref icones

--- resources/views/loisirs.blade.php
+++ resources/views/loisirs.blade.php
@@ -10,18 +10,18 @@
 @section('content')
 	<div class="loisirs"><a href="https://www.mixcloud.com/vito-sanacho/">DJ</a> dans des hôtels,clubs et soirées privées (2005-2012) </div>
 	<div class="sport">Sports : basket et footing en amateur</div>
-		
-	<i class="github icon" style="font-size: 6em; "></i>
-	<i class="gitlab icon" style="font-size: 6em;"></i>
-	<i class="facebook icon" style="font-size: 6em;"></i>
-	<i class="instagram icon" style="font-size: 6em;"></i>
-	<i class="linkedin square icon" style="font-size: 6em;"></i>
-	<i class="mixcloud icon" style="font-size: 6em;"></i>
-	<i class="snapchat ghost icon" style="font-size: 6em;"></i>
-	<i class="snapchat icon" style="font-size: 6em;"></i>
-	<i class="snapchat square icon" style="font-size: 6em;"></i>
-	<i class="soundcloud icon" style="font-size: 6em;"></i>
-	<i class="twitter icon" style="font-size: 6em;"></i>
-	<i class="twitter square icon" style="font-size: 6em;"></i>
-	<i class="whatsapp icon" style="font-size: 6em;"></i>
+
+	<a href=""><i class="github icon" style="font-size: 6em; "></i></a>
+	<a href=""><i class="gitlab icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="facebook icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="instagram icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="linkedin square icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="mixcloud icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="snapchat ghost icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="snapchat icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="snapchat square icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="soundcloud icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="twitter icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="twitter square icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="whatsapp icon" style="font-size: 6em;"></i></a>
 @endsection
