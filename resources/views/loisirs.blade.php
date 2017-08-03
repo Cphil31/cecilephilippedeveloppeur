605957689848ce4e74ed1edf82972c1bf7d706d4
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 23:06:30 +0200

    ajout des icones dans loisirs

--- resources/views/loisirs.blade.php
+++ resources/views/loisirs.blade.php
@@ -10,4 +10,17 @@
 @section('content')
 	<div class="loisirs"><a href="https://www.mixcloud.com/vito-sanacho/">DJ</a> dans des hôtels,clubs et soirées privées (2005-2012) </div>
 	<div class="loisirs">Sports : basket et footing en amateur</div>
+	<i class="github icon"></i>
+	<i class="gitlab icon"></i>
+	<i class="facebook icon"></i>
+	<i class="instagram icon"></i>
+	<i class="linkedin square icon"></i>
+	<i class="mixcloud icon"></i>
+	<i class="snapchat ghost icon"></i>
+	<i class="snapchat icon"></i>
+	<i class="snapchat square icon"></i>
+	<i class="soundcloud icon"></i>
+	<i class="twitter icon"></i>
+	<i class="twitter square icon"></i>
+	<i class="whatsapp icon"></i>
 @endsection
