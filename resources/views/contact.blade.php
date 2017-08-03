e5567eac45ce10259d0cc2e75062827c944d98d3
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 18:51:05 +0200

    formulaire centré

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,30 +11,39 @@
 
 <h1>Contactez-nous</h1>
 
-<div class="ui form success">
-	<div class="two fields">
-		<div class="field">
-			<label>Nom</label>
-			<input placeholder="John" type="text">
+<div class="ui grid">
+<div class="two wide column"></div>
+	<div class="twelve wide column">
+		<div class="ui form success">
+			<div class="two fields">
+				<div class="field">
+					<label>Nom</label>
+					<input placeholder="John" type="text">
+				</div>
+				<div class="field">
+					<label>Prenom</label>
+					<input placeholder="Doe" type="text">
+				</div>
+			</div>
+			<div class="field">
+				<label>E-mail</label>
+				<input type="email" placeholder="johndoe@schmoe.com">
+			</div>
+			<div class="field">
+				<label>Object</label>
+				<textarea rows="1"></textarea>
+			</div>
+			<div class="field">
+				<label>Message</label>
+				<textarea></textarea>
+			</div>
+			<div class="ui submit button">Envoi</div>
 		</div>
-		<div class="field">
-			<label>Prenom</label>
-			<input placeholder="Doe" type="text">
-		</div>
 	</div>
-	<div class="field">
-		<label>E-mail</label>
-		<input type="email" placeholder="johndoe@schmoe.com">
-	</div>
-	<div class="field">
-		<label>Object</label>
-		<textarea rows="2"></textarea>
-	</div>
-	<div class="field">
-		<label>texte</label>
-		<textarea></textarea>
-	</div>
-	<div class="ui submit button">Envoi</div>
+	<div class="four wide column"></div>
 </div>
+
+
+
 
 @endsection
