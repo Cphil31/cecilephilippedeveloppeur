cac8a9c712756382caa240d8b6ebfb6c00a9295d
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 23:32:12 +0200

    mise en page triicones

--- resources/views/loisirs.blade.php
+++ resources/views/loisirs.blade.php
@@ -11,17 +11,13 @@
 	<div class="loisirs"><a href="https://www.mixcloud.com/vito-sanacho/">DJ</a> dans des hôtels,clubs et soirées privées (2005-2012) </div>
 	<div class="sport">Sports : basket et footing en amateur</div>
 
+	<a href=""><i class="linkedin square icon" style="font-size: 6em;"></i></a>
 	<a href=""><i class="github icon" style="font-size: 6em; "></i></a>
 	<a href=""><i class="gitlab icon" style="font-size: 6em;"></i></a>
+	<a href=""><i class="slack icon" style="font-size: 6em;" ></i></a>
+	<a href=""><i class="twitter square icon" style="font-size: 6em;"></i></a>
 	<a href=""><i class="facebook icon" style="font-size: 6em;"></i></a>
 	<a href=""><i class="instagram icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="linkedin square icon" style="font-size: 6em;"></i></a>
 	<a href=""><i class="mixcloud icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="snapchat ghost icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="snapchat icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="snapchat square icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="soundcloud icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="twitter icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="twitter square icon" style="font-size: 6em;"></i></a>
-	<a href=""><i class="whatsapp icon" style="font-size: 6em;"></i></a>
+
 @endsection
