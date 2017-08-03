b5fc2d24d3d7226068bed26600260ddc4dd24681
Cphil31 <cecilephilippe31@gmail.com>
2017-08-03 18:46:14 +0200

    ajout du formulaire contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,12 +3,38 @@
 @section('title', 'Page Title')
 
 @section('sidebar')
-    @parent
-    
+@parent
+
 @endsection
 
 @section('content')
-    
-<h1>Page des contacts</h1>
+
+<h1>Contactez-nous</h1>
+
+<div class="ui form success">
+	<div class="two fields">
+		<div class="field">
+			<label>Nom</label>
+			<input placeholder="John" type="text">
+		</div>
+		<div class="field">
+			<label>Prenom</label>
+			<input placeholder="Doe" type="text">
+		</div>
+	</div>
+	<div class="field">
+		<label>E-mail</label>
+		<input type="email" placeholder="johndoe@schmoe.com">
+	</div>
+	<div class="field">
+		<label>Object</label>
+		<textarea rows="2"></textarea>
+	</div>
+	<div class="field">
+		<label>texte</label>
+		<textarea></textarea>
+	</div>
+	<div class="ui submit button">Envoi</div>
+</div>
 
 @endsection
