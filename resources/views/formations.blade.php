acb33724c60a46836bd913a4a8e4b651cfc09452
Cphil31 <cecilephilippe31@gmail.com>
2017-08-04 13:22:05 +0200

    formations recadré

--- resources/views/formations.blade.php
+++ resources/views/formations.blade.php
@@ -11,20 +11,23 @@
 
 <div class="formations">	
 	<div class="ui grid">
-		<div class="four wide column">2016-2017</div>
-		<div class="nine wide column">Formation de Web Développeur à l'école Simplon</div>
+		<div class="two wide column"></div>
+		<div class="three wide column">2016-2017</div>
+		<div class="seven wide column">Formation de Web Développeur à l'école Simplon</div>
 		<div class="two wide column">Toulouse</div>
 	</div>
 
 	<div class="ui grid">
-		<div class="four wide column">2007 - 2008</div>
-		<div class="nine wide column">Introduction au Droit Immobilier CNAM</div>
+		<div class="two wide column"></div>
+		<div class="three wide column">2007 - 2008</div>
+		<div class="seven wide column">Introduction au Droit Immobilier CNAM</div>
 		<div class="two wide column">Guadeloupe</div>
 	</div>
 
 	<div class="ui grid">
-		<div class="four wide column">2003</div>
-		<div class="nine wide column">Baccalaurréat STT</div>
+		<div class="two wide column"></div>
+		<div class="three wide column">2003</div>
+		<div class="seven wide column">Baccalaurréat STT</div>
 		<div class="two wide column">Guadeloupe</div>
 	</div>
 </div>
