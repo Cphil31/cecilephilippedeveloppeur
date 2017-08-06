3d362ec39a5d34b7176c63e463d9971d071ef6de
Cphil31 <cecilephilippe31@gmail.com>
2017-08-06 14:37:19 +0200

    creation controller contact

--- resources/views/test_contact.blade.php
+++ resources/views/test_contact.blade.php
@@ -12,14 +12,14 @@
 <h1>Test contact</h1>
 <div class="ui grid">
   <div class="four wide column"></div>
-  <div class="four wide column">
+  <div class="eight wide column">
 			{!! Form::open(['url' => 'users']) !!}
 			        {!! Form::label('nom', 'Entrez votre nom : ') !!}
 			        {!! Form::text('nom') !!}
 			        {!! Form::submit('Envoyer !') !!}
 			    {!! Form::close() !!}
   </div>
-  <div class="four wide column"></div>
+  <div class=" wide column"></div>
 </div>
 
 @endsection
