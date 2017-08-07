82df72b76abe2baedc591a367c2fcbb920145739
Cphil31 <cecilephilippe31@gmail.com>
2017-08-07 14:10:37 +0200

    lien cv pdf ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,8 @@
     
 <div class="texthome">Développeur web junior à la recherche d'un emploi </div>
 
-<a href="/../views/cv.CECILE.Philippe.pdf" download="Curriculum_Vitae_CECILE_Philippe">téléchargez mon Cv en PDF</a>
-<a href="CVCECILEPhilippe"></a>
+<div class="texthome">
+<a href="https://rawgit.com/Cphil31/cecilephilippedeveloppeur/master/resources/views/CV_CECILE_Philippe.pdf" download="Curriculum_Vitae_CECILE_Philippe">Telechargez mon Cv en PDF</a>
+</div>
+
 @endsection
