ead8b1412a3fca0f66f5af9bdf34a1b8cce8b6c4
Cphil31 <cecilephilippe31@gmail.com>
2017-08-15 14:24:02 +0200

    contact centré

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,10 +12,12 @@
 		<div class="textcontact">
 			<a href="https://rawgit.com/Cphil31/cecilephilippedeveloppeur/master/resources/views/cv.CECILE.Philippe.pdf" download="Curriculum_Vitae_CECILE_Philippe">Telechargez mon Cv en PDF</a>
 		</div>
+
+
 <div class="ui grid contact">
-	<div class="two wide column"></div>
+	<div class="three wide column"></div>
 	<div class="twelve wide column">
-		<a href="mailto:cecilephilippe31@gmail.com" ><i class="mail outline icon" style="margin-left:auto;margin-right: auto ; font-size:6em;"></i></a>	
+		<a href="mailto:cecilephilippe31@gmail.com" ><i class="mail outline icon" style="margin-left:auto;margin-right:auto; font-size:6em;"></i></a>	
 		<a href="https://www.linkedin.com/in/philippe-cecile-a579b212b/" target="_blank"><i class="linkedin square icon" style="font-size:6em;"></i></a>
 		<a href="https://github.com/Cphil31" target="_blank"><i class="github icon" style="font-size:6em;"></i></a>
 		<a href="https://about.gitlab.com/" target="_blank"><i class="gitlab icon" style="font-size:6em;"></i></a>
