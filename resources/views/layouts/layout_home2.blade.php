1c074eefe9be481f9c93fd92ee693b18d1abbef6
Cphil31 <cecilephilippe31@gmail.com>
2017-08-23 15:42:21 +0200

    ajout animation

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -140,7 +140,7 @@
 <div class="competences">Utilisation des langages de programmation PHP,SQL,Javascript,Css,HTML</div>
   <div class="competences">Utilisation des Progammes: SublimText,Node.js,Express.js</div>
   <div class="competences">Analyse d'un cahier des charges</div>
-  <div class="competences">Utilisation des CMS et du Framework (<a href="http://getbootstrap.com/">Boostrap</a> ,<a href="http://foundation.zurb.com/">Foundation</a>,<a href="http://semantic-ui.com/">Semantic</a>)</div>
+  <div class="competences">Utilisation des CMS et du Framework (<a href="http://getbootstrap.com/">Bootstrap</a> ,<a href="http://foundation.zurb.com/">Foundation</a>,<a href="http://semantic-ui.com/">Semantic</a>)</div>
 
 <!-- LOISIRS -->
 
