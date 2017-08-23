64b5a58d020a58347c0911289b4bffa38c312d73
Cphil31 <cecilephilippe31@gmail.com>
2017-08-23 17:50:23 +0200

    mise en page

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -74,8 +74,8 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">Septembre 2016- Mars 2017</div>
-    <div class="six wide column">Développement de projets Web à l'école Simplon (évènements : <a href="https://hourofcode.com/fr">Hour of code</a>, <a href="http://www.niort-numeric.fr/hackathon/">Hackathon 2016</a>)</div>
-    <div class="three wide column">Toulouse</div>
+    <div class="seven wide column">Développement de projets Web à l'école Simplon (évènements : <a href="https://hourofcode.com/fr">Hour of code</a>, <a href="http://www.niort-numeric.fr/hackathon/">Hackathon 2016</a>)</div>
+    <div class="two wide column">Toulouse</div>
   </div>
 
 
@@ -83,7 +83,7 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">Juillet 2015-Septembre 2016</div>
-    <div class="six wide column">Agent de démantèlemenent - Envie</div>
+    <div class="sevtwoen wide column">Agent de démantèlemenent - Envie</div>
     <div class="three wide column">Portet-sur-Garonne</div>
   </div>
 
@@ -92,8 +92,8 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">Septembre 2014 - juillet 2015</div>
-    <div class="six wide column">Chauffeur livreur - La pie verte</div>
-    <div class="three wide column">Cornebarrieu</div>
+    <div class="seven wide column">Chauffeur livreur - La pie verte</div>
+    <div class="two wide column">Cornebarrieu</div>
   </div>        
 
 
@@ -101,16 +101,16 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">2011 - Septembre 2014</div>
-    <div class="six wide column">En quête de reconversion</div>
-    <div class="three wide column">Toulouse</div>
+    <div class="seven wide column">En quête de reconversion</div>
+    <div class="two wide column">Toulouse</div>
   </div>
 
 
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">2010 - 2011</div>
-    <div class="six wide column">Agent d'accueil / Livreur - CCAS</div>
-    <div class="three wide column">Guadeloupe</div>
+    <div class="seven wide column">Agent d'accueil / Livreur - CCAS</div>
+    <div class="two wide column">Guadeloupe</div>
   </div>
 
 
@@ -119,7 +119,7 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">2005 - 2010</div>
-    <div class="six wide column">Commercial - Caraïbes Immo Pro</div>
+    <div class="seven wide column">Commercial - Caraïbes Immo Pro</div>
     <div class="three wide column">Guadeloupe</div>
   </div>
 
@@ -128,7 +128,7 @@
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="three wide column">2005 - 2010</div>
-    <div class="six wide column">Prestations Dj pour Clubs, Hotels et particuliers</div>
+    <div class="seven wide column">Prestations Dj pour Clubs, Hotels et particuliers</div>
     <div class="three wide column">Guadeloupe</div>
   </div>
   <div class="four wide column"></div>  
