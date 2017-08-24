f515728d89437903bfcbd90d12229782ab6058c3
Cphil31 <cecilephilippe31@gmail.com>
2017-08-24 22:43:53 +0200

    ajout grid form

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -172,15 +172,23 @@
 </div>
 
 <!-- FORMULAIRE -->
-<div class="form">  
-  <form action="/contact" method="POST">
-    {{ csrf_field() }}
-    
-    <input type="text" name="nom">
-    <input type="text" name="mail">
-    <textarea name="text" id="" cols="30" rows="10"></textarea>
-    <button type="submit"></button>
-  </form>
+<div class="ui grid">
+  <div class="three wide column"></div>
+  <div class="ten wide column">
+
+    <div class="form">  
+      <form action="/contact" method="POST">
+        {{ csrf_field() }}
+        
+        <input type="text" name="nom">
+        <input type="text" name="mail">
+        <textarea name="text" id="" cols="30" rows="10"></textarea>
+        <button type="submit"></button>
+      </form>
+    </div>
+
+  </div>
+  <div class="four wide column"></div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
