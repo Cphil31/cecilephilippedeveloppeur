d52ebfcadb5b1311bae0aa8ff035f1489e6fbdef
Cphil31 <cecilephilippe31@gmail.com>
2017-08-26 13:35:42 +0200

    mise en page

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -10,15 +10,15 @@
   <link rel="stylesheet" href="../css/app.css">
 </head>
 <body>
+      <div class="titre">CECILE Philippe</div>
+      <!-- <hr> -->
+      <div class="titre">Developpeur Web</div>
+     <div class="spaceTitle"></div>
   <!-- MENU -->
   <div class="ui grid">
     <div class="two wide column"></div>
     <div class="twelve wide column">
 
-      <div class="titre">CECILE Philippe</div>
-      <hr>
-      <div class="titre">Developpeur Web</div>
-     <div class="spaceTitle"></div>
       <!-- MENU -->
       <div class="ui blue five item inverted menu">
         <a class="formationMenu item">Formations</a>
@@ -210,9 +210,11 @@
     </div>
   <div class="four wide column"></div>
 </div>
-<div class="container">
+    <div class="container">
         @yield('content')
     </div>
+<footer></footer>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
