5ad2aa645fb8cf0fef4f4e81e0c4c5ff8fc32c4c
Cphil31 <cecilephilippe31@gmail.com>
2017-09-27 10:37:15 +0200

    mail to icon

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -167,7 +167,7 @@
   <div class="twelve wide column">
 
     <div class="icon">
-      <a href="https://github.com/Cphil31" target="_blank"><i class="github icon" style="font-size:6em;color:white"></i></a>
+      <a href="https://github.com/Cphil31/cecilephilippedeveloppeur" target="_blank"><i class="github icon" style="font-size:6em;color:white"></i></a>
       <a  href="https://www.linkedin.com/in/philippe-cecile-a579b212b/" target="_blank"><i class="linkedin square icon" style="font-size:6em;color:white"></i></a>
       <a href="mailto:cecilephilippe31@gmail.com" ><i class="mail outline icon" style="margin-left:auto;margin-right:auto; font-size:6em;color:white;"></i></a> 
       <a href="https://gitlab.com/Cecile_philippe" target="_blank"><i class="gitlab icon" style="font-size:6em;color:white"></i></a>
