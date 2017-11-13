abd5f581346e4c0816b87b3cf71097d877f20dca
Cphil31 <cecilephilippe31@gmail.com>
2017-11-13 14:37:18 +0100

    media queries

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -8,6 +8,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/app.css">
+  <link rel="stylesheet" media="screen and (max-width: 1280px)" href="../css/petite_resolution.css"
 </head>
 <body>
   <div class="space"></div>
