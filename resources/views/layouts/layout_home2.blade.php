b10cb388a0e746c34a2623c7491fdac881d11f14
Cphil31 <cecilephilippe31@gmail.com>
2018-02-15 16:37:47 +0100

    mise en page

--- resources/views/layouts/layout_home2.blade.php
+++ resources/views/layouts/layout_home2.blade.php
@@ -252,6 +252,10 @@
   @yield('content')
 </div>
 <div class="footer">
+
+   <div class="row">
+    <div class="col"></div>
+    <div class="col-5">
   <div class="icon">Suivez-moi sur :
     <a href="https://github.com/Cphil31/cecilephilippedeveloppeur" target="_blank"><i class="github icon" style="color:white"></i></a>
     <a  href="https://www.linkedin.com/in/philippe-cecile-a579b212b/" target="_blank"><i class="linkedin square icon" style="color:white"></i></a>
@@ -261,8 +265,19 @@
     <a  href="https://www.facebook.com/DonSanacho" target="_blank"><i class="facebook icon" style="color:white;"></i></a>
     <a href="https://www.instagram.com/donsanacho" target="_blank"><i class="instagram icon" style="color:white"></i></a>
     <a href="https://www.mixcloud.com/vito-sanacho/" target="_blank"><i class="mixcloud icon" style="color:white"></i></a>
+    
   </div>
+      
+    </div>
+    <div class="col">
+        <div class="source">image source :
+        <a class="source" href="https://picsum.photos/" style="color:white;" target="_blank"> picsum photo</a>
+        </div>
+    </div>
+  </div>
+  
 </div>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
