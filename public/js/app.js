0461d8c86974a031815fb67b457491648eff1e73
Cphil31 <cecilephilippe31@gmail.com>
2018-04-05 13:44:21 +0200

    mise a jour temps

--- public/js/app.js
+++ public/js/app.js
@@ -11,14 +11,14 @@ $(".titre").hide();
 $(".developpeur").hide();
 $(".gauche").hide();
 $(".droite").hide();
-$(".titre").delay(3500).show(1500);
-$(".menu").transition('hide');
-$(".menu").transition('fly left','3500ms');
-$(".footer").transition('hide');
-$(".developpeur").delay(5000).show(2000);
-$(".gauche").delay(7000).show(2000);
-$(".droite").delay(9000).show(2000);
-$(".footer").transition('fly right','3500ms');
+$(".titre").delay(1000).show(1500);
+// $(".menu").transition('hide');
+// $(".menu").transition('fly left','3500ms');
+$(".developpeur").delay(3000).show(2000);
+$(".gauche").delay(5000).show(2000);
+$(".droite").delay(7000).show(2000);
+// $(".footer").transition('hide');
+// $(".footer").transition('fly right','3500ms');
 
 $('.acceuilMenu').click(function(){
 	$(".formations").transition('hide');
