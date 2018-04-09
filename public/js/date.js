518eefef9f991ef04c87748d90fe8f8daa77ad56
Cphil31 <cecilephilippe31@gmail.com>
2018-04-09 23:05:17 +0200

    0 de  date ajouté

--- public/js/date.js
+++ public/js/date.js
@@ -3,4 +3,13 @@ var maintenant=new Date();
 var jour=maintenant.getDate();
 var mois=maintenant.getMonth()+1;
 var an=maintenant.getFullYear();
+if (jour.length < 2 );
+{
+	jour="0"+jour;
+}
+if (mois.length < 2 );
+{
+	mois="0"+mois;
+}
+
 $('.date').html(jour + '/' + mois + '/' + an);
