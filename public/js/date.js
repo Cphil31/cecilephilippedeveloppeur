f5725d4508f94a95b8098d60c69a227262011dda
Cphil31 <cecilephilippe31@gmail.com>
2018-04-10 01:06:33 +0200

    fonction date ok

--- public/js/date.js
+++ public/js/date.js
@@ -1,15 +1,20 @@
-console.log("affichage date");
+function aujourdhui(){
+
 var maintenant=new Date();
 var jour=maintenant.getDate();
 var mois=maintenant.getMonth()+1;
 var an=maintenant.getFullYear();
-if (jour.length < 2 );
+
+if (jour.length <2);
 {
-	jour="0"+jour;
+	jour=jour;
 }
-if (mois.length < 2 );
+if (mois.length<2);
 {
 	mois="0"+mois;
 }
+$('.date').html(jour + '/' +  mois + '/' + an);
 
-$('.date').html(jour + '/' + mois + '/' + an);
+};
+
+aujourdhui();
