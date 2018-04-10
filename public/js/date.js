b0b193f4a4f16091cc1a2b4e9e301ad6fb39cbfc
Cphil31 <cecilephilippe31@gmail.com>
2018-04-10 02:00:47 +0200

    mise en page

--- public/js/date.js
+++ public/js/date.js
@@ -1,29 +1,25 @@
 
 function aujourdhui(){
 
-var maintenant=new Date();
-var jour=maintenant.getDate();
-var mois=maintenant.getMonth()+1;
-var an=maintenant.getFullYear();
-console.log(jour);
-if (jour>=10)
-{
-	$('.date').html(jour+'/'+mois+'/'+an);
-}
-else{
-	$('.date').html("0"+jour+'/'+mois+'/'+an);
-}
-if (mois>=10)
-{
-	$('.date').html(jour+'/'+mois+'/'+an);
-}
-else{
-	$('.date').html(jour+'/'+"0"+mois +'/'+an);
-}
-
-
-
+	var maintenant=new Date();
+	var jour=maintenant.getDate();
+	var mois=maintenant.getMonth()+1;
+	var an=maintenant.getFullYear();
+	console.log(jour);
+	if (jour>=10)
+	{
+		$('.date').html(jour+'/'+mois+'/'+an);
+	}
+	else{
+		$('.date').html("0"+jour+'/'+mois+'/'+an);
+	}
+	if (mois>=10)
+	{
+		$('.date').html(jour+'/'+mois+'/'+an);
+	}
+	else{
+		$('.date').html(jour+'/'+"0"+mois +'/'+an);
+	}
 };
 
 aujourdhui();
-
