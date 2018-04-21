94b9f3ef3f8cfdbf7435286172c12adf15cb2855
Cphil31 <cecilephilippe31@gmail.com>
2018-04-22 00:56:39 +0200

    date et heure centré

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
   <div class="row">
         <div class="col"></div>
         <div class="col- date"></div>
-        <div class="col-3"></div>
+        <div class="col-7"></div>
         <div class="col- clock"></div>
         <div class="col"></div>
    </div>
