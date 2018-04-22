cd340aecdcae1654cfb2cd36001155c750e27ee6
Cphil31 <cecilephilippe31@gmail.com>
2018-04-22 18:05:34 +0200

    boutons pour la transition ok

--- public/js/nav.js
+++ public/js/nav.js
@@ -7,13 +7,33 @@ $(".formulaire").transition('hide');
 
 };
 hide();
+
 $(".formationMenu").click(function(){
 	$(".acceuil").transition('hide');
 	hide();
 	$(".formations").transition('fly right','1000ms');
-	});
+});
+
 $(".competenceMenu").click(function(){
 	$(".acceuil").transition('hide');
 	hide();
 	$(".competences").transition('fly right','1000ms');
-	});
+});
+
+$(".experienceMenu").click(function(){
+	$(".acceuil").transition('hide');
+	hide();
+	$(".experiences").transition('fly right','1000ms');
+});
+
+$(".loisirMenu").click(function(){
+	$(".acceuil").transition('hide');
+	hide();
+	$(".loisirs").transition('fly right','1000ms');
+});
+
+$(".contactMenu").click(function(){
+	$(".acceuil").transition('hide');
+	hide();
+	$(".formulaire").transition('fly right','1000ms');
+});
