df320e93a07861c545ccd0f61af52c522f7e3613
Cphil31 <cecilephilippe31@gmail.com>
2018-04-22 14:50:07 +0200

    j'efface toute les transistions

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,100 +1,26 @@
 
 $(".formationMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".experiences").transition('hide');
-	$(".competences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".rightload").transition('hide');
-	$(".form").transition('hide');
-	$(".textFormation").transition('hide');
-	$(".contactezmoi").transition('hide');
-	$(".segment").transition('hide');
+
+
 	$(".formations").transition('fly right','1000ms');
 	});
 
 $(".competenceMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".experiences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".rightload").transition('hide');
-	$(".form").transition('hide');
-	$(".contactezmoi").transition('hide');
-	$("formulaire").hide();
-	$(".segment").transition('hide');
-	$(".apple").transition('fly right','1000ms');
-	$(".windows").transition('fly right','1000ms');
-	$(".linux").transition('fly right','1000ms');
-	$(".html5").transition('fly right','1000ms');
-	$(".css3").transition('fly right','1000ms');
-	$(".competences").transition('fly right','1000ms');
-	});
+	
 
 $(".experienceMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".competences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".rightload").transition('hide');
-	$(".form").transition('hide');
-	$(".segment").transition('hide');
-	$(".contactezmoi").transition('hide');
-	$(".experiences").transition('fly right','1000ms');
+	
 });
 
 $(".loisirMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".competences").transition('hide');
-	$(".experiences").transition('hide');
-	$(".rightload").transition('hide');
-	$(".form").transition('hide');
-	$(".contactezmoi").transition('hide');
-	$(".loisirs").transition('fly right','1000ms');
-	$(".sport").transition('fly right','1000ms');
-	$(".segment").transition('hide');
+	
 	});
 
 $(".contactMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".competences").transition('hide');
-	$(".experiences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".spacecomp").transition('hide');
-	$(".segment").transition('fly right','1000ms');
-	$(".contactezmoi").transition('fly right','1000ms');
-	$(".form").transition('fly right','1000ms');
-	$(".download").transition('fly right','1000ms');
-		});
+	
 
 $(".contactMenu").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".competences").transition('hide');
-	$(".experiences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".spacecomp").transition('hide');
-	$(".segment").transition('fly right','1000ms');
-	// $(".contactezmoi").transition('fly right','1000ms');
-	// $(".form").transition('fly right','1000ms');
-	$(".download").transition('fly right','1000ms');
+	
 		});
 $(".cont").click(function(){
-	$(".acceuil").transition('hide');
-	$(".formations").transition('hide');
-	$(".competences").transition('hide');
-	$(".experiences").transition('hide');
-	$(".loisirs").transition('hide');
-	$(".sport").transition('hide');
-	$(".spacecomp").transition('hide');
-	$(".segment").transition('fly right','1000ms');
-	$(".contactezmoi").transition('fly right','1000ms');
-	$(".form").transition('fly right','1000ms');
-	$(".download").transition('fly right','1000ms');
-		});
+	});
